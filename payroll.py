862b9ffd0e9598a9118c8f7db1db193b89e4995b
Gibran Shah <gshah@assertivesolutions.ca>
2026-08-11 22:12:49 -0600

    Move code for calculating federal tax into function

--- payroll.py
+++ payroll.py
@@ -68,6 +68,74 @@ def get_tax_bracket(annual_taxable_income, brackets):
             return rate, constant
 
     raise ValueError("No tax bracket found")
+    
+def calculate_federal_tax(payroll, employee_cpp, cra):
+    pay_periods = cra["pay_periods"]
+
+    # T4127 factor F5:
+    # Deductible additional CPP contribution
+    cpp_first_additional_rate = cra["cpp_first_additional_rate"]
+
+    f5 = employee_cpp * (cpp_first_additional_rate / cra["cpp_rate"])
+
+    # T4127 Step 1:
+    # Annual taxable income (A)
+    annual_taxable_income = pay_periods * (payroll - f5)
+
+    federal_rate, federal_constant = get_tax_bracket(
+        annual_taxable_income,
+        cra["federal_brackets"]
+    )
+
+    # T4127 factor K1:
+    # Federal non-refundable personal tax credit
+    federal_claim_amount = cra["federal_claim_amount"]
+
+    k1 = 0.15 * federal_claim_amount
+
+    # T4127 factor K2:
+    # Federal tax credit for base CPP contributions
+    base_cpp_rate = 0.0495
+    max_annual_base_cpp = cra["max_annual_base_cpp"]
+
+    annual_base_cpp = min(
+        base_cpp_rate * ((pay_periods * payroll) - 3500),
+        max_annual_base_cpp
+    )
+
+    k2 = 0.15 * annual_base_cpp
+
+    # T4127 factor K4:
+    # Federal Canada Employment Amount tax credit
+    canada_employment_amount = cra["canada_employment_amount"]
+
+    k4 = min(
+        0.15 * (pay_periods * payroll),
+        0.15 * canada_employment_amount
+    )
+
+    # T4127 Step 2:
+    # Basic federal tax
+    k3 = 0.00
+
+    t3 = (
+        federal_rate * annual_taxable_income
+        - federal_constant
+        - k1
+        - k2
+        - k3
+        - k4
+    )
+
+    # T4127 Step 3:
+    # Annual federal tax payable
+    lcf = 0.00
+
+    t1 = max(0, t3 - (pay_periods * lcf))
+
+    federal_tax = t1 / pay_periods
+
+    return federal_tax, annual_taxable_income, annual_base_cpp
 
 total = float(input("Enter total including GST: $"))
 
@@ -91,68 +159,11 @@ employee_cpp = calculate_cpp(
 
 employer_cpp = employee_cpp
 
-pay_periods = cra["pay_periods"]
-
-# T4127 factor F5:
-# Deductible additional CPP contribution
-cpp_first_additional_rate = cra["cpp_first_additional_rate"]
-
-f5 = employee_cpp * (cpp_first_additional_rate / cpp_rate)
-
-# T4127 Step 1:
-# Annual taxable income (A)
-annual_taxable_income = pay_periods * (payroll - f5)
-
-federal_rate, federal_constant = get_tax_bracket(
-    annual_taxable_income,
-    cra["federal_brackets"]
+federal_tax, annual_taxable_income, annual_base_cpp = calculate_federal_tax(
+    payroll,
+    employee_cpp,
+    cra
 )
-    
-# T4127 factor K1:
-# Federal non-refundable personal tax credit
-federal_claim_amount = cra["federal_claim_amount"]
-
-k1 = 0.15 * federal_claim_amount
-
-# T4127 factor K2:
-# Federal tax credit for base CPP contributions
-base_cpp_rate = 0.0495
-max_annual_base_cpp = cra["max_annual_base_cpp"]
-
-annual_base_cpp = min(
-    base_cpp_rate * ((pay_periods * payroll) - 3500),
-    max_annual_base_cpp
-)
-
-k2 = 0.15 * annual_base_cpp
-
-# T4127 factor K4:
-# Federal Canada Employment Amount tax credit
-canada_employment_amount = cra["canada_employment_amount"]
-
-k4 = min(
-    0.15 * (pay_periods * payroll),
-    0.15 * canada_employment_amount
-)
-
-# T4127 Step 2:
-# Basic federal tax
-k3 = 0.00
-
-t3 = (
-    federal_rate * annual_taxable_income
-    - federal_constant
-    - k1
-    - k2
-    - k3
-    - k4
-)
-
-# T4127 Step 3:
-# Annual federal tax payable
-lcf = 0.00
-
-t1 = max(0, t3 - (pay_periods * lcf))
 
 # T4127 Step 4:
 # Select Alberta tax rate (V) and constant (KP)
@@ -193,8 +204,7 @@ t2 = t4
 # Tax deductions for the monthly pay period
 additional_tax = 0.00  # Factor L
 
-federal_tax = t1 / pay_periods
-alberta_tax = t2 / pay_periods
+alberta_tax = t2 / cra["pay_periods"]
 
 total_tax = federal_tax + alberta_tax + additional_tax
 
@@ -205,18 +215,18 @@ print(f"Earned: ${earned:,.2f}")
 print(f"Payroll:                ${payroll:,.2f}")
 print(f"Employee CPP:           ${employee_cpp:,.2f}")
 print(f"Employer CPP:           ${employer_cpp:,.2f}")
-print(f"Additional CPP (F5):     ${f5:,.2f}")
+#print(f"Additional CPP (F5):     ${f5:,.2f}")
 print(f"Annual taxable income A: ${annual_taxable_income:,.2f}")
-print(f"Federal rate (R):         {federal_rate:.1%}")
-print(f"Federal constant (K):     ${federal_constant:,.2f}")
-print(f"Federal claim amount (TC): ${federal_claim_amount:,.2f}")
-print(f"Federal tax credit (K1):   ${k1:,.2f}")
+#print(f"Federal rate (R):         {federal_rate:.1%}")
+#print(f"Federal constant (K):     ${federal_constant:,.2f}")
+#print(f"Federal claim amount (TC): ${federal_claim_amount:,.2f}")
+#print(f"Federal tax credit (K1):   ${k1:,.2f}")
 print(f"Annual base CPP:          ${annual_base_cpp:,.2f}")
-print(f"Federal CPP credit (K2):  ${k2:,.2f}")
-print(f"Canada Employment Amount: ${canada_employment_amount:,.2f}")
-print(f"Federal employment credit (K4): ${k4:,.2f}")
-print(f"Basic federal tax (T3):   ${t3:,.2f}")
-print(f"Annual federal tax (T1):  ${t1:,.2f}")
+#print(f"Federal CPP credit (K2):  ${k2:,.2f}")
+#print(f"Canada Employment Amount: ${canada_employment_amount:,.2f}")
+#print(f"Federal employment credit (K4): ${k4:,.2f}")
+#print(f"Basic federal tax (T3):   ${t3:,.2f}")
+#print(f"Annual federal tax (T1):  ${t1:,.2f}")
 print(f"Alberta rate (V):          {alberta_rate:.1%}")
 print(f"Alberta constant (KP):     ${alberta_constant:,.2f}")
 print(f"Alberta claim amount (TCP): ${alberta_claim_amount:,.2f}")
